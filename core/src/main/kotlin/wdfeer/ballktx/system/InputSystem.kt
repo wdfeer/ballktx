250ce5265708a9f229194d5f4dd4b69bfdeab8d2
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-09-06 11:22:57 +0200

    Add extra inputs

--- core/src/main/kotlin/wdfeer/ballktx/system/InputSystem.kt
+++ core/src/main/kotlin/wdfeer/ballktx/system/InputSystem.kt
@@ -3,6 +3,9 @@ package wdfeer.ballktx.system
 import com.badlogic.ashley.core.EntitySystem
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
+import com.badlogic.gdx.Input.Keys
+import com.badlogic.gdx.math.Vector2
+import ktx.math.times
 import wdfeer.ballktx.component.BodyComponent
 
 class InputSystem : EntitySystem() {
@@ -11,15 +14,37 @@ class InputSystem : EntitySystem() {
     }
 
     override fun update(deltaTime: Float) {
-        val force: Float = 5f
+        Gdx.input.handle()
+    }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.W))
-            engine.entities[0]!!.getComponent(BodyComponent::class.java).body.applyForceToCenter(0f, force, true)
-        if (Gdx.input.isKeyPressed(Input.Keys.A))
-            engine.entities[0]!!.getComponent(BodyComponent::class.java).body.applyForceToCenter(-force, 0f, true)
-        if (Gdx.input.isKeyPressed(Input.Keys.S))
-            engine.entities[0]!!.getComponent(BodyComponent::class.java).body.applyForceToCenter(0f, -force, true)
-        if (Gdx.input.isKeyPressed(Input.Keys.D))
-            engine.entities[0]!!.getComponent(BodyComponent::class.java).body.applyForceToCenter(force, 0f, true)
+    private fun Input.handle() {
+        val camera = engine.getSystem(RenderSystem::class.java).camera
+
+        if (isKeyPressed(Keys.EQUALS))
+            camera.zoom += 0.003f
+        if (isKeyPressed(Keys.MINUS))
+            camera.zoom -= 0.003f
+
+        val forceMagnitude: Float = if (isKeyPressed(Keys.SHIFT_LEFT)) 15f else 5f
+        val forceVector = Vector2(0f, 0f)
+
+        if (isKeyPressed(Keys.W))
+            forceVector.y += forceMagnitude
+        if (isKeyPressed(Keys.A))
+            forceVector.x -= forceMagnitude
+        if (isKeyPressed(Keys.S))
+            forceVector.y -= forceMagnitude
+        if (isKeyPressed(Keys.D))
+            forceVector.x += forceMagnitude
+
+        val ball = engine.entities[0]!!
+        val ballBody = ball.getComponent(BodyComponent::class.java).body
+
+        if (isKeyPressed(Keys.CONTROL_LEFT)) {
+            ballBody.linearVelocity *= 0.99f
+            ballBody.angularVelocity *= 0.99f
+        }
+
+        ballBody.applyForceToCenter(forceVector, true)
     }
 }
