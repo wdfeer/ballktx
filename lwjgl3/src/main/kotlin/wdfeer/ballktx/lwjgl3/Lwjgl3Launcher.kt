778a597ee379db563d93557e3028345d5783bbd4
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-09-06 09:41:48 +0200

    Increase default resolution

--- lwjgl3/src/main/kotlin/wdfeer/ballktx/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/wdfeer/ballktx/lwjgl3/Lwjgl3Launcher.kt
@@ -13,7 +13,7 @@ fun main() {
       return
     Lwjgl3Application(Main(), Lwjgl3ApplicationConfiguration().apply {
         setTitle("ballktx")
-        setWindowedMode(640, 480)
+        setWindowedMode(1600, 900)
         setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
     })
 }
