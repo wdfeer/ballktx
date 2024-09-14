9d4cccaa3547a1ce850e23834c02abe367bca427
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-09-14 10:23:23 +0200

    Implement connecting new rooms

--- core/src/main/kotlin/wdfeer/ballktx/entity/Wall.kt
+++ core/src/main/kotlin/wdfeer/ballktx/entity/Wall.kt
@@ -8,11 +8,11 @@ import com.badlogic.gdx.physics.box2d.World
 import ktx.assets.disposeSafely
 import wdfeer.ballktx.component.BodyComponent
 
-class Wall(world: World, pos: Vector2, size: Vector2) : Entity() {
+class Wall(world: World, val position: Vector2, size: Vector2) : Entity() {
     init {
         val shape = PolygonShape().apply { setAsBox(size.x / 2f, size.y / 2f) }
 
-        add(BodyComponent(world, pos, BodyDef.BodyType.StaticBody) {
+        add(BodyComponent(world, position, BodyDef.BodyType.StaticBody) {
             createFixture(shape, 0f)
             userData = this@Wall
         })
--- core/src/main/kotlin/wdfeer/ballktx/system/RoomSystem.kt
+++ core/src/main/kotlin/wdfeer/ballktx/system/RoomSystem.kt
@@ -30,7 +30,7 @@ class RoomSystem(eng: Engine) : EntitySystem() {
     }
 }
 
-class Room(val engine: Engine, val world: World, val center: Vector2, val size: Vector2, openLeftWall: Boolean = false) { // TODO: use the open argument to determine whether the left wall should be split open
+class Room(val engine: Engine, private val world: World, val center: Vector2, val size: Vector2, openLeftWall: Boolean = false) {
     private fun Engine.addWall(world: World, position: Vector2, size: Vector2): Wall =
         Wall(world, position, size).also { this.addEntity(it) }
     private fun removeWall(wall: Wall) {
@@ -39,40 +39,99 @@ class Room(val engine: Engine, val world: World, val center: Vector2, val size: 
         world.destroyBody(wall.body)
     }
 
-    val walls: MutableList<Wall> = spawnWalls(engine, world, center, size, openLeftWall).toMutableList()
+    private val walls: MutableList<Wall> = spawnWalls(engine, world, center, size, openLeftWall).toMutableList()
 
     private val wallThickness = 1f
     private fun spawnWalls(engine: Engine, world: World, center: Vector2, size: Vector2, openLeftWall: Boolean): List<Wall> {
         val width = size.x
         val height = size.y
-        return listOf(
-            // TODO: Split left wall into 3 equal walls without the middle one if 'openLeftWall' is true
-            engine.addWall(
+        val wallList = mutableListOf<Wall>()
+
+        // Left wall
+        if (openLeftWall) {
+            val wallHeight = height / 3
+            wallList.add(engine.addWall(
+                world,
+                Vector2(center.x - width / 2 - wallThickness / 2, center.y - height / 3),
+                Vector2(wallThickness, wallHeight)
+            ))
+            wallList.add(engine.addWall(
+                world,
+                Vector2(center.x - width / 2 - wallThickness / 2, center.y + height / 3),
+                Vector2(wallThickness, wallHeight)
+            ))
+        } else {
+            wallList.add(engine.addWall(
                 world,
                 Vector2(center.x - width / 2 - wallThickness / 2, center.y),
                 Vector2(wallThickness, height)
-            ),
-            // TODO: Split this right wall into 3 equal walls
-            engine.addWall(
-                world,
-                Vector2(center.x + width / 2 + wallThickness / 2, center.y),
-                Vector2(wallThickness, height)
-            ),
-            engine.addWall( // Bottom wall
-                world,
-                Vector2(center.x, center.y - height / 2 - wallThickness / 2),
-                Vector2(width, wallThickness)
-            ),
-            engine.addWall( // Top wall
-                world,
-                Vector2(center.x, center.y + height / 2 + wallThickness / 2),
-                Vector2(width, wallThickness)
-            )
-        )
+            ))
+        }
+
+        // Right wall
+        val rightWallHeight = height / 3
+        wallList.add(engine.addWall(
+            world,
+            Vector2(center.x + width / 2 + wallThickness / 2, center.y - height / 3),
+            Vector2(wallThickness, rightWallHeight)
+        ))
+        wallList.add(engine.addWall(
+            world,
+            Vector2(center.x + width / 2 + wallThickness / 2, center.y),
+            Vector2(wallThickness, rightWallHeight)
+        ))
+        wallList.add(engine.addWall(
+            world,
+            Vector2(center.x + width / 2 + wallThickness / 2, center.y + height / 3),
+            Vector2(wallThickness, rightWallHeight)
+        ))
+
+        // Bottom wall
+        wallList.add(engine.addWall(
+            world,
+            Vector2(center.x, center.y - height / 2 - wallThickness / 2),
+            Vector2(width, wallThickness)
+        ))
+
+        // Top wall
+        wallList.add(engine.addWall(
+            world,
+            Vector2(center.x, center.y + height / 2 + wallThickness / 2),
+            Vector2(width, wallThickness)
+        ))
+
+        return wallList
     }
 
     fun connect(other: Room) {
-        TODO("Remove middle right wall of this, then create a corridor connecting the rooms")
+        // Remove middle right wall of this room
+        val middleRightWall = walls.find { it.position.x > center.x && it.position.y == center.y }
+        if (middleRightWall != null) {
+            removeWall(middleRightWall)
+        }
+
+        // Create a corridor connecting the rooms
+        val corridorWidth = 20f
+        val corridorHeight = size.y / 3
+        val corridorCenter = Vector2(
+            (center.x + other.center.x) / 2,
+            center.y
+        )
+
+        val corridorWalls = listOf(
+            engine.addWall(
+                world,
+                Vector2(corridorCenter.x, corridorCenter.y + corridorHeight / 2 + wallThickness / 2),
+                Vector2(other.center.x - center.x - size.x, wallThickness)
+            ),
+            engine.addWall(
+                world,
+                Vector2(corridorCenter.x, corridorCenter.y - corridorHeight / 2 - wallThickness / 2),
+                Vector2(other.center.x - center.x - size.x, wallThickness)
+            )
+        )
+
+        walls.addAll(corridorWalls)
     }
 
     fun getRandomPosition() = center + size * Vector2(Random.nextFloat() - 0.5f, Random.nextFloat() - 0.5f)
