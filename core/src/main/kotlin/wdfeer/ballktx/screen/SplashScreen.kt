bb1317002c61db17af6a41f8068c17d783765754
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-09-14 09:54:22 +0200

    Update SplashScreen

--- core/src/main/kotlin/wdfeer/ballktx/screen/SplashScreen.kt
+++ core/src/main/kotlin/wdfeer/ballktx/screen/SplashScreen.kt
@@ -1,5 +1,6 @@
 package wdfeer.ballktx.screen
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import ktx.app.KtxScreen
@@ -16,14 +17,19 @@ class SplashScreen : KtxScreen {
     private val batch = SpriteBatch()
 
     override fun render(delta: Float) {
-        clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
+        clearScreen(red = 0.1f, green = 0.1f, blue = 0.1f)
         batch.use {
-            it.draw(image, 100f, 160f)
+            it.draw(image, Gdx.graphics.width / 2f - LOGO_WIDTH / 2f, Gdx.graphics.height / 2f - LOGO_HEIGHT / 2f)
         }
     }
 
     override fun dispose() {
         image.disposeSafely()
         batch.disposeSafely()
+    }
+
+    companion object {
+        private const val LOGO_WIDTH = 440
+        private const val LOGO_HEIGHT = 160
     }
 }
