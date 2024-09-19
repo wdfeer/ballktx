4e8d1d2fc119e7f0ad1ec7009fd4dd3a46a0bd35
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-09-19 17:14:56 +0200

    Create Label entity

---
+++ core/src/main/kotlin/wdfeer/ballktx/entity/Label.kt
@@ -0,0 +1,18 @@
+package wdfeer.ballktx.entity
+
+import com.badlogic.ashley.core.Entity
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.math.Vector2
+import wdfeer.ballktx.component.DrawComponent
+
+class Label(private val text: String, private val position: Vector2) : Entity() {
+    init {
+        add(DrawComponent {
+            font.draw(it, text, position.x, position.y)
+        })
+    }
+
+    companion object {
+        val font = BitmapFont()
+    }
+}
